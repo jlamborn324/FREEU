ff44e6ff921b53101ba20e3afb4e3589728b7967
Joseph Lamborn <jl4922a@student.american.edugit config --global user.name Joseph>
2020-02-01 14:33:19 -0500

    front end stuff

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ItemPost
 
-# Register your models here.
+class ItemPostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "condition", "date_posted")
+
+admin.site.register(ItemPost, ItemPostAdmin)
