2d9a7df8700b645bbcc09fd4886132f7ea1c35fd
Kate Straneva <katestraneva@Kates-MacBook-Pro-2.local>
2020-02-01 16:07:04 -0500

    please

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name= "marketplace-home"),
+    path('itempost/<int:pk>/', PostDetailView.as_view(), name= "itempost-detail"),
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import ItemPost
 
 
@@ -28,6 +28,8 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
+class PostDetailView(ListView): 
+    model = ItemPost
 
 
 
