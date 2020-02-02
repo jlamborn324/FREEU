d7d8a23dfe764612702fedfba8932d413ff6725f
Joseph Lamborn <jl4922a@student.american.edugit config --global user.name Joseph>
2020-02-01 19:59:05 -0500

    added pictures to accompany posts

--- marketplace/models.py
+++ marketplace/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 class ItemPost(models.Model):
     title = models.CharField(max_length=100)
-    # item_image = ImageField(upload_to='#', blank=True, null=True)
+    item_image = models.ImageField(blank=True, null=True)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     condition = models.CharField(max_length=20)
