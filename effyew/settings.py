115deb3750e0fba7aa641c517b4df2b44d938855
Joseph Lamborn <jl4922a@student.american.edugit config --global user.name Joseph>
2020-02-02 09:23:30 -0500

    import statement

--- effyew/settings.py
+++ effyew/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
@@ -143,7 +143,6 @@ LOGIN_URL = 'login'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
-
 
 
 
