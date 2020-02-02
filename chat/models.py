56518131c13f6ac962d613a6e2a0da01f46156eb
Joseph Lamborn <jl4922a@student.american.edugit config --global user.name Joseph>
2020-02-02 02:48:40 -0500

    commit 4 kate

--- chat/models.py
+++ chat/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 User = get_user_model()
 
+
 class Message(models.Model):
     author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
     content = models.TextField()
