1f504e8f9434087670b57ab56207a6b5be3846ea
Joseph Lamborn <jl4922a@student.american.edugit config --global user.name Joseph>
2020-04-16 00:29:10 -0400

    test, getting rid of usages of django-heroku

--- effyew/settings.py
+++ effyew/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # TEMPLATES = [
@@ -146,4 +146,4 @@ MEDIA_ROOT = 'media'
 
 
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
