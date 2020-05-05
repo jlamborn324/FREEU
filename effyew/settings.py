bbb27135f56b6214704cf816a0c6feba1013e549
Joseph Lamborn <jl4922a@student.american.edugit config --global user.name Joseph>
2020-05-05 16:27:54 -0400

    Added password reset functionality

--- effyew/settings.py
+++ effyew/settings.py
@@ -151,3 +151,11 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_S3_REGION_NAME = 'us-east-2' #change to your region
 AWS_S3_SIGNATURE_VERSION = 's3v4'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
